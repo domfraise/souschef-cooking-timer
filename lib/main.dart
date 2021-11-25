efb075e954c8a82eb2f6e1416cc9cafad68a5a78
Dominic Fraise <dominic.fraise@transferwise.com>
2021-11-25 13:30:55 +0000

    remove some comments

--- lib/main.dart
+++ lib/main.dart
@@ -93,7 +93,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   String targetCounter = "0";
 
   void _incrementCounter() {
@@ -120,28 +119,10 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Row(
@@ -166,7 +147,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 }
 
                 final data = snapshot.data!;
-                DateTime? date = DateTime.tryParse(data["current_date"]);
                 return Expanded(
                     child: Column(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -185,7 +165,6 @@ class _MyHomePageState extends State<MyHomePage> {
                               TextField(
                                   textCapitalization: TextCapitalization.sentences,
                                   textAlign: TextAlign.center,
-                                  // controller: TextEditingController(),
                                   onChanged: (value) {
                                     targetCounter = value;
                                     setState(() {});
@@ -202,7 +181,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.play_arrow),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
